0bb30ea0f4d5676e7804df543ad3e56cdd9ee670
Alexey Sh <AlexeyShv992@gmail.com>
2021-03-16 23:41:01 +0300

    fix prompt in src/Game.php

--- src/Game.php
+++ src/Game.php
@@ -8,16 +8,16 @@ use function cli\prompt;
 function game(callable $getParams)
 {
     line('Welcome to the Brain Games!');
-    $name = prompt('May I have your name? ');
+    $name = prompt('May I have your name? ', false, '');
     line('Hello, %s!', $name);
     [$target, $getTask, $rounds] = $getParams();
     line($target);
     for ($i = 0; $i < $rounds; $i++) {
         [$question, $answer] = $getTask();
         line("Question: {$question}");
-        $playerAnswer = mb_strtolower(trim(prompt('Your answer: ')));
+        $playerAnswer = mb_strtolower(trim(prompt('Your answer: ', false, '')));
         if ($playerAnswer !== $answer) {
-            line("{$playerAnswer} is wrong answer ;(. Correct answer was '{$answer}'");
+            line("{$playerAnswer} is wrong answer ;(. Correct answer was '{$answer}'.");
             line("Let's try again, {$name}");
             return;
         }
