474cb5a9252bf837660028b0bb03656e7e3f6026
Alexey Sh <AlexeyShv992@gmail.com>
2021-03-17 20:55:30 +0300

    del old code from src/Games/Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -5,26 +5,6 @@ namespace Brain\Games\Games\Even;
 use function Brain\Games\Game\game;
 use function Brain\Games\Utils\Random\genRandNumber;
 
-function even_old()
-{
-    $target = 'Answer "yes" if the number is even, otherwise answer "no".';
-    $rounds = 3;
-    $isEven = function ($num) {
-        return $num % 2 === 0;
-    };
-    $getTask = function () use ($isEven) {
-        $minNum = 1;
-        $maxNum = 100;
-        $question = random_int($minNum, $maxNum);
-        $answer = $isEven($question) ? 'yes' : 'no';
-        return [$question, $answer];
-    };
-    $getParams = function () use ($target, $getTask, $rounds) {
-        return [$target, $getTask, $rounds];
-    };
-    game($getParams);
-}
-
 function isEven($num)
 {
     return $num % 2 === 0;
